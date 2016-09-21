5d7eb335ea46fceaa14390a6e5368c28fabd38a8
Matthew Keefe <matt@pixelbit.com>
2016-09-20 22:07:06 -0400

    Create NinetyNineBottles.swift

---
+++ NinetyNineBottles.swift
@@ -0,0 +1,27 @@
+public class NinetyNineBottles {
+    
+    let beer:Int = 99
+    
+    func playSong() {
+        
+        for i in (1...beer).reverse() {
+            
+            let next = i == 1 ? 0 : (i-1)
+            
+            print("\(i) \(self.bottleString(i)) of beer on the wall, \(i) \(self.bottleString(i)) of beer.\n"
+                + "Take one down and pass it around, "
+                + "\(next) \(self.bottleString(i-1))" + " of beer on the wall.")
+            
+        }
+        
+    }
+    
+    func bottleString(beer:Int) -> String {
+        return (beer == 1) ? "bottle" : "bottles"
+    }
+    
+}
+
+// Play 99 Bottles of Beer
+let beer = NinetyNineBottles()
+beer.playSong()
