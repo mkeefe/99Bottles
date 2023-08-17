f58a62a4047bad302ec64518efd743358e613649
Matthew Keefe <matt@pixelbit.com>
2023-08-16 21:31:32 -0400

    Update to Swift5

--- NinetyNineBottles.swift
+++ NinetyNineBottles.swift
@@ -1,23 +1,23 @@
 public class NinetyNineBottles {
     
-    let beer:Int = 99
+    let beer: Int = 99
     
     func playSong() {
         
-        for i in (1...beer).reverse() {
+        for i in (1...beer).reversed() {
             
-            let next = i == 1 ? 0 : (i-1)
+            let next = i == 1 ? 0 : (i - 1)
             
-            print("\(i) \(self.bottleString(i)) of beer on the wall, \(i) \(self.bottleString(i)) of beer.\n"
+            print("\(i) \(self.bottleString(beer: i)) of beer on the wall, \(i) \(self.bottleString(beer: i)) of beer.\n"
                 + "Take one down and pass it around, "
-                + "\(next) \(self.bottleString(i-1))" + " of beer on the wall.")
+                + "\(next) \(self.bottleString(beer: i - 1))" + " of beer on the wall.")
             
         }
         
     }
     
-    func bottleString(beer:Int) -> String {
-        return (beer == 1) ? "bottle" : "bottles"
+    func bottleString(beer: Int) -> String {
+        return beer == 1 ? "bottle" : "bottles"
     }
     
 }
