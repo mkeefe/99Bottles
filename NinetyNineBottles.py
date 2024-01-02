0e310300b483c419fd0309a6a26b348edb34c93a
Matthew Keefe <matt@pixelbit.com>
2024-01-02 16:54:20 -0500

    Update NinetyNineBottles.py

--- NinetyNineBottles.py
+++ NinetyNineBottles.py
@@ -9,11 +9,12 @@ def playSong():
 
     for i in range(beer, 0, -1) :
        
-        next = 0 if i == 1 else (i-1)
+        next = "no more" if i == 1 else i - 1
+        nextBottles = bottleString(0) if next == "no more" else bottleString(next)
 
         print(f"{i} {bottleString(i)} of beer on the wall, {i} {bottleString(i)} of beer.\n" 
               + f"Take one down and pass it around, " 
-              + f"{next} {bottleString(i-1)} or beer on the wall.")
+              + f"{next} {nextBottles} or beer on the wall.")
         
     print("No more bottles of beer on the wall, no more bottles of beer.")
     print("There's nothing else to fall, because there's no more bottles of beer on the wall.")
