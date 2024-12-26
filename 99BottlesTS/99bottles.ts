99f0fc8fb619b72b9a44087e8d10a83cc111f99f
Matt Keefe <matt@pixelbit.com>
2024-12-26 18:12:52 -0500

    bug: ensure usage of console.info() only

--- 99BottlesTS/99bottles.ts
+++ 99BottlesTS/99bottles.ts
@@ -7,11 +7,11 @@ const playSong = () => {
     let bottleString = getBottleString(i);
     let lineOfSong = `${i} ${bottleString} of beer on the wall, ${i} ${bottleString} of beer. `
       + `Take one down and pass it around, ${i-1} ${getBottleString(i-1)} of beer on the wall.`;
-    console.log(lineOfSong);
+    console.info(lineOfSong);
   }
   console.info('No more bottles of beer on the wall, no more bottles of beer.');
 };
 
 console.info('[Start song]');
 playSong();
-console.log('[End Song]');
+console.info('[End Song]');
